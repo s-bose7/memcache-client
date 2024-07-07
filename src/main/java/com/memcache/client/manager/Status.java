4325c7389c7b656ff80f1c8446d0061e4ce91108
Sudip Bose <sbose007ime.work@gmail.com>
2024-07-07 15:32:29 +0530

    Add: Status enumeration for cache state evaluation

--- src/main/java/com/memcache/client/manager/Status.java
+++ src/main/java/com/memcache/client/manager/Status.java
@@ -1,5 +1,19 @@
 package com.memcache.client.manager;
 
 public enum Status {
+  /*
+   * Uninitialized, indicates it is not ready for use.
+   */
+  UNINITIALIZED,
+
+  /*
+   * Maintenance, indicates exclusive access to allow for restricted operations.
+   */
+  MAINTENANCE,
+
+  /*
+   * Available, indicates it is ready for use.
+   */
+  AVAILABLE;
 
 }
