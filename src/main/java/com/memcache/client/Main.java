c8a13e82eafb0abdd26904972f9f60b6ce9d3dc0
Sudip Bose <sbose007ime.work@gmail.com>
2024-07-07 15:31:17 +0530

    Update: Cache initialization demo

--- src/main/java/com/memcache/client/Main.java
+++ src/main/java/com/memcache/client/Main.java
@@ -1,25 +1,26 @@
 package com.memcache.client;
 
+import com.memcache.client.manager.CacheManager;
+import com.memcache.client.config.CacheConfiguration;
+import com.memcache.client.config.ConfigurationBuilder;
+
 
 public class Main {
-	public static void main(String[] args) {
 		
-    	MemCache memCache = new MemCache(10);
-    	memCache.put(120, 345);
-    	memCache.put(121, 346);
-    	memCache.put(122, 347);
-    	memCache.put(123, 348);
-
-    	System.out.println("Key: 120, Value: " + memCache.get(120)); // 345
-    	System.out.println("Key: 121, Value: " + memCache.get(121)); // 346
-    	System.out.println("Key: 122, Value: " + memCache.get(122)); // 347
-    	System.out.println("Key: 123, Value: " + memCache.get(123)); // 348
-
-    	boolean preRemovalStatus = memCache.exists(123);
-    	boolean postRemovalStatus= memCache.remove(123);  	
-    	
-    	if(preRemovalStatus == true && postRemovalStatus == true) {
-    		System.out.println("Success!");
-    	}
+	public static void main(String[] args) {
+		CacheConfiguration config = new ConfigurationBuilder()
+													.setCacheName("db-cache-id-5432")
+													.setInitialSize(100)
+													.setExpirationTime(60)
+													.setLoggingEnabled(false)
+													.setCompressionEnabled(false)
+													.build();		
+		CacheManager cacheManager = CacheManager.getInstance(config);
+		Cache memCache = cacheManager.getCache();
+		
+		memCache.put(500, 2);
+		System.out.println(memCache.get(500));         // 2
+		System.out.println(memCache.remove(500));      // true
+		System.out.println(memCache.containsKey(500)); // false
 	}
 }
