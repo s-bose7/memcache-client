9b8e759728c746ccb2f8fcaa9371538abfd08b6b
Sudip Bose <sbose007ime.work@gmail.com>
2024-07-08 16:01:21 +0530

    Add: Exception handling in the cache interface and implementation

--- src/main/java/com/memcache/client/Cache.java
+++ src/main/java/com/memcache/client/Cache.java
@@ -1,5 +1,11 @@
 package com.memcache.client;
 
+
+import com.memcache.client.exception.CacheBulkReadingException;
+import com.memcache.client.exception.CacheBulkWritingException;
+import com.memcache.client.exception.CacheReadingException;
+import com.memcache.client.exception.CacheWritingException;
+
 import java.util.Map;
 import java.util.Set;
 
@@ -26,15 +32,19 @@ public interface Cache {
      * 
      * @param key the key with which the specified value is to be associated
      * @param value the value to be associated with the specified key
+     * @throws CacheWritingException if an error occurs while writing the value for the given key 
+     *                               to the underlying native library.
      */
-    public void put(int key, int value);
+    public void put(int key, int value) throws CacheWritingException;
 
     /**
      * Maps all the specified entries in this cache.
      * 
      * @param entries the entries to be associated with their respective keys
+     * @throws CacheBulkWritingException if an error occurs while writing multiple values 
+     *                                   to the underlying native library.
      */
-    public void putAll(Map<Integer, Integer> entries);
+    public void putAll(Map<Integer, Integer> entries) throws CacheBulkWritingException;
 
     /**
      * Maps the specified key to the specified value in this cache,
@@ -43,33 +53,44 @@ public interface Cache {
      * @param key the key with which the specified value is to be associated
      * @param value the value to be associated with the specified key
      * @return the existing value associated with the key, or null if there was no mapping for the key
+     * @throws CacheReadingException if an error occurs while reading the existing value for the given key 
+     *                               from the underlying native library.
+     * @throws CacheWritingException if an error occurs while writing the value for the given key 
+     *                               to the underlying native library.
      */
-    public int putIfAbsent(int key, int value);
+    public int putIfAbsent(int key, int value) throws CacheReadingException, CacheWritingException;
 
     /**
      * Returns the value to which the specified key is mapped,
-     * or null if this cache contains no mapping for the key.
+     * or default value type if this cache contains no mapping for the key.
      * 
      * @param key the key whose associated value is to be returned
-     * @return the value to which the specified key is mapped, or null if there is no mapping for the key
+     * @return the value to which the specified key is mapped, or default value type, 
+     *                               if there is no mapping for the key.
+     * @throws CacheReadingException if an error occurs while reading the value for the given key 
+     *                               from the underlying native library.
      */
-    public int get(int key);
+    public int get(int key) throws CacheReadingException;
 
     /**
      * Returns a map of the values to which the specified keys are mapped.
      * 
      * @param keys the keys whose associated values are to be returned
      * @return a map of the values to which the specified keys are mapped
+     * @throws CacheBulkReadingException if an error occurs while reading multiple values 
+     *                                   from the underlying native library.
      */
-    public Map<Integer, Integer> getAll(Set<Integer> keys);
+    public Map<Integer, Integer> getAll(Set<Integer> keys) throws CacheBulkReadingException;
 
     /**
      * Removes the mapping for the specified key from this cache if present.
      * 
      * @param key the key whose mapping is to be removed from the cache
      * @return true if the mapping was removed, false otherwise
+     * @throws CacheWritingException if an error occurs while removing the value for the given key 
+     *                               from the underlying native library.
      */
-    public boolean remove(int key);
+    public boolean remove(int key) throws CacheWritingException;
 
     /**
      * Removes the entry for the specified key only if it is currently mapped to the specified value and is not expired.
@@ -77,15 +98,19 @@ public interface Cache {
      * @param key the key whose mapping is to be removed
      * @param value the value expected to be associated with the key
      * @return true if the value was removed, false otherwise
+     * @throws CacheWritingException if an error occurs while removing the value for the given key 
+     *                               from the underlying native library.
      */
-    public boolean remove(int key, int value);
+    public boolean remove(int key, int value) throws CacheWritingException;
 
     /**
      * Removes the mappings for the specified keys from this cache.
      * 
      * @param keys the keys whose mappings are to be removed from the cache
+     * @throws CacheBulkWritingException if an error occurs while removing multiple values 
+     *                                   from the underlying native library.
      */
-    public void removeAll(Set<Integer> keys);
+    public void removeAll(Set<Integer> keys) throws CacheBulkWritingException;
 
     /**
      * Replaces the entry for the specified key only if it is currently mapped to some value and the entry is not expired.
@@ -93,8 +118,12 @@ public interface Cache {
      * @param key the key with which the specified value is to be associated
      * @param value the value to be associated with the specified key
      * @return the previous value associated with the key, or null if there was no mapping for the key
+     * @throws CacheReadingException if an error occurs while reading the existing value for the given key 
+     *                               from the underlying native library.
+     * @throws CacheWritingException if an error occurs while writing the value for the given key 
+     *                               to the underlying native library.
      */
-    public int replace(int key, int value);
+    public int replace(int key, int value) throws CacheReadingException, CacheWritingException;
 
     /**
      * Replaces the entry for the specified key only if it is currently mapped to the old value and the entry is not expired.
@@ -103,6 +132,10 @@ public interface Cache {
      * @param oldValue the value expected to be associated with the key
      * @param newValue the value to be associated with the specified key
      * @return true if the value was replaced, false otherwise
+     * @throws CacheReadingException if an error occurs while reading the existing value for the given key 
+     *                               from the underlying native library.
+     * @throws CacheWritingException if an error occurs while writing the value for the given key 
+     *                               to the underlying native library.
      */
-    public boolean replace(int key, int oldValue, int newValue);
+    public boolean replace(int key, int oldValue, int newValue) throws CacheReadingException, CacheWritingException;
 }
--- src/main/java/com/memcache/client/MemCache.java
+++ src/main/java/com/memcache/client/MemCache.java
@@ -1,31 +1,35 @@
 package com.memcache.client;
 
+import com.memcache.client.core.NativeCache;
+import com.memcache.client.config.CacheConfiguration;
+
+import com.memcache.client.exception.CacheBulkReadingException;
+import com.memcache.client.exception.CacheBulkWritingException;
+import com.memcache.client.exception.CacheInitializationException;
+import com.memcache.client.exception.CacheReadingException;
+import com.memcache.client.exception.CacheWritingException;
+
 import com.sun.jna.Pointer;
 
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Set;
 
-import com.memcache.client.core.NativeCache;
-import com.memcache.client.config.CacheConfiguration;
-
-
 public class MemCache implements Cache {
-	
+
 	private int TTL;
 	private int capacity;
 
 	private Pointer nativeCachePtr;
 	private CacheConfiguration cacheConfig;
-	
-	
-	public MemCache(CacheConfiguration config) {
+
+	public MemCache(CacheConfiguration config) throws CacheInitializationException {
 		cacheConfig = config;
 		TTL = cacheConfig.getExpirationTimeInSeconds();
-		
+
 		capacity = cacheConfig.getInitialSize();
-		if(capacity <= 0) {
-			throw new IllegalArgumentException("Capacity must be non negative");
+		if (capacity <= 0) {
+			throw new CacheInitializationException("Capacity must be non negative");
 		}
 		nativeCachePtr = NativeCache.INSTANCE.init(capacity);
 	}
@@ -41,75 +45,124 @@ public class MemCache implements Cache {
 	}
 
 	@Override
-	public void put(int key, int value) {
-		NativeCache.INSTANCE.put(nativeCachePtr, key, value, TTL);
-	}
-
-	@Override
-	public void putAll(Map<Integer, Integer> entries) {
-		for(Map.Entry<Integer, Integer> entry : entries.entrySet()) {
-			put(entry.getKey(), entry.getValue());
+	public void put(int key, int value) throws CacheWritingException {
+		try {
+			NativeCache.INSTANCE.put(nativeCachePtr, key, value, TTL);
+		} catch (Exception e) {
+			throw new CacheWritingException("Error writing to cache for key: " + key, e);
 		}
 	}
 
 	@Override
-	public int putIfAbsent(int key, int value) {
-		if(!containsKey(key)) {
-			put(key, value);
-			return value;
+	public void putAll(Map<Integer, Integer> entries) throws CacheBulkWritingException {
+
+		for (Map.Entry<Integer, Integer> entry : entries.entrySet()) {
+			try {
+				put(entry.getKey(), entry.getValue());
+			} catch (CacheWritingException e) {
+				throw new CacheBulkWritingException("Error writing multiple entries to cache", e);
+			}
 		}
-		return get(key);
 	}
 
 	@Override
-	public int get(int key) {
-		int value = NativeCache.INSTANCE.get(nativeCachePtr, key);
-		return value;
+	public int putIfAbsent(int key, int value) throws CacheReadingException, CacheWritingException {
+		try {
+			if (!containsKey(key)) {
+				put(key, value);
+				return value;
+			} else {
+				return get(key);
+			}
+		} catch (CacheReadingException e) {
+			throw new CacheReadingException("Error reading from cache for key: " + key, e);
+		} catch (CacheWritingException e) {
+			throw new CacheWritingException("Error writing to cache for key: " + key, e);
+		}
 	}
 
 	@Override
-	public Map<Integer, Integer> getAll(Set<Integer> keys) {
+	public int get(int key) throws CacheReadingException {
+		try {
+			return NativeCache.INSTANCE.get(nativeCachePtr, key);
+		} catch (Exception e) {
+			throw new CacheReadingException("Error reading from cache for key: " + key, e);
+		}
+	}
+
+	@Override
+	public Map<Integer, Integer> getAll(Set<Integer> keys) throws CacheBulkReadingException {
 		HashMap<Integer, Integer> mappings = new HashMap<>();
-		for(int key : keys) {
-			mappings.put(key, get(key));
+		for (int key : keys) {
+			try {
+				int mappedValue = get(key);
+				mappings.put(key, mappedValue);
+			} catch (CacheReadingException e) {
+				throw new CacheBulkReadingException("Error reading multiple entries from cache", e);
+			}
 		}
 		return mappings;
 	}
 
 	@Override
-	public boolean remove(int key) {
-		return NativeCache.INSTANCE.removeFromCache(nativeCachePtr, key);
-	}
-
-	@Override
-	public boolean remove(int key, int value) {
-		if(containsKey(key) && get(key) == value) {
-			return remove(key);
-		}
-		return false;
-	}
-
-	@Override
-	public void removeAll(Set<Integer> keys) {
-		for(int key : keys) {
-			remove(key);
+	public boolean remove(int key) throws CacheWritingException {
+		try {
+			return NativeCache.INSTANCE.removeFromCache(nativeCachePtr, key);
+		} catch (Exception e) {
+			throw new CacheWritingException("Error writing to cache for key: " + key, e);
 		}
 	}
 
 	@Override
-	public int replace(int key, int value) {
-		int oldValue = get(key);
-		put(key, value);
-		return oldValue;
-	}
-
-	@Override
-	public boolean replace(int key, int oldValue, int newValue) {
-		int mappedValue = get(key);
-		if (mappedValue != (oldValue)) {
+	public boolean remove(int key, int value) throws CacheWritingException {
+		try {
+			if (containsKey(key) && get(key) == value) {
+				return remove(key);
+			}
 			return false;
+		} catch (CacheReadingException e) {
+			throw new CacheWritingException("Error reading from cache for key: " + key, e);
 		}
-		put(key, newValue);
-		return true;
+	}
+
+	@Override
+	public void removeAll(Set<Integer> keys) throws CacheBulkWritingException {
+		for (int key : keys) {
+			try {
+				remove(key);
+			} catch (CacheWritingException e) {
+				throw new CacheBulkWritingException("Error removing multiple entries from cache", e);
+			}
+		}
+	}
+
+	@Override
+	public int replace(int key, int value) throws CacheReadingException, CacheWritingException {
+		try {
+			int oldValue = get(key);
+			put(key, value);
+			return oldValue;
+		} catch (CacheReadingException e) {
+			throw new CacheReadingException("Error reading from cache for key: " + key, e);
+		} catch (CacheWritingException e) {
+			throw new CacheWritingException("Error writing to cache for key: " + key, e);
+		}
+	}
+
+	@Override
+	public boolean replace(int key, int oldValue, int newValue) throws CacheReadingException, CacheWritingException {
+
+		try {
+			int mappedValue = get(key);
+			if (mappedValue != oldValue) {
+				return false;
+			}
+			put(key, newValue);
+			return true;
+		} catch (CacheReadingException e) {
+			throw new CacheReadingException("Error reading from cache for key: " + key, e);
+		} catch (CacheWritingException e) {
+			throw new CacheWritingException("Error writing to cache for key: " + key, e);
+		}
 	}
 }
