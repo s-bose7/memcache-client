b26cf096557bab64bd1253af1ca6877dc8ac9649
Sudip Bose <sbose007ime.work@gmail.com>
2024-07-08 15:58:46 +0530

    Add: Access control modifiers for native methods

--- src/main/java/com/memcache/client/core/NativeCache.java
+++ src/main/java/com/memcache/client/core/NativeCache.java
@@ -4,16 +4,18 @@ import com.sun.jna.Library;
 import com.sun.jna.Native;
 import com.sun.jna.Pointer;
 
+
 public interface NativeCache extends Library {
 	
-	NativeCache INSTANCE = (NativeCache) Native.load("MemCache", NativeCache.class);
+	public NativeCache INSTANCE = (NativeCache) Native.load("MemCache", NativeCache.class);
 	
-	Pointer init(int capacity);
-	void deleteCache(Pointer cache);
 	
-	int get(Pointer cache, int key);
-	void put(Pointer cache, int key, int value, int ttl);
-    boolean removeFromCache(Pointer cache, int key);
-    boolean clear(Pointer cache);
-    boolean exists(Pointer cache, int key);	
+	public Pointer init(int capacity);
+	public void deleteCache(Pointer cache);
+	
+	public int get(Pointer cache, int key);
+	public void put(Pointer cache, int key, int value, int ttl);
+	public boolean removeFromCache(Pointer cache, int key);
+	public boolean clear(Pointer cache);
+	public boolean exists(Pointer cache, int key);	
 }
